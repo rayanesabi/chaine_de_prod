973a59ed6602bbde80821ece7a6b05feb40ac9ce
rayanesabi <92162339+rayanesabi@users.noreply.github.com>
2023-12-11 11:44:20 +0100

    Update app.py

--- web/app.py
+++ web/app.py
@@ -1,14 +1,17 @@
-from flask import Flask
+from flask import request, Flask
+import json
 
-app=Flask(__name__)
-
-@app.route("/")
-
-def hello_world():
-	return "Hello from Flask !"
-
-
-
-if __name__ == '__main__' :
-	app.run(debug=True,host='0.0.0.0', port=5000)
+app = Flask(__name__)
+@app.route('/plus_one')
+def plus_one():
+ x = int(request.args.get('x', 1))
+ return json.dumps({'x': x + 1})
+@app.route('/plus_two')
+def plus_two():
+ x = int(request.args.get('x', 1))
+ return json.dumps({'x': x + 2})
+@app.route('/square')
+def square():
+ x = int(request.args.get('x', 1))
+ return json.dumps({'x': x * x})
 
